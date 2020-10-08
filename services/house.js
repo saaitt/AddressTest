a600167b91b3ca5fbffdc53f9fa7939ca3eb9777
saaitt <saeid.firouzi@gmail.com>
2020-10-08 11:42:12 +0330

    polygonSearch

--- services/house.js
+++ services/house.js
@@ -15,5 +15,29 @@ async function nearbyHousesWithin500m(latitude, longitude) {
     })
     return houses
 }
-
+async function polygonSearch(arrayOfLocations) {
+    let polygonQuery = await makePolygonQuery(arrayOfLocations)
+    let query = `SELECT  id, age, rent, latitude, longitude, mortgage 
+                    FROM "Houses"  
+                    WHERE ST_Contains(` + polygonQuery + `,ST_MakePoint(longitude, latitude)) IS TRUE`
+    let houses = await db.sequelize.query(query, {
+        type: db.Sequelize.QueryTypes.SELECT
+    })
+    return houses
+}
+async function makePolygonQuery(arrayOfLocations) {
+    let polygonLocations = arrayOfLocations
+    polygonLocations.push(polygonLocations[0])
+    let ArrayOfPoints = 'ARRAY['
+    for (let i = 0; i < polygonLocations.length; i++) {
+        let point = 'ST_MakePoint(' + polygonLocations[i].longitude + ',' + polygonLocations[i].latitude + ')'
+        if (i < polygonLocations.length - 1)
+            ArrayOfPoints = ArrayOfPoints.concat(point, ",")
+        else
+            ArrayOfPoints = ArrayOfPoints.concat(point, "]")
+    }
+    let query = `ST_MakePolygon(ST_MakeLine(` + ArrayOfPoints + `))`
+    return query
+}
 module.exports.nearbyHousesWithin500m = nearbyHousesWithin500m
+module.exports.polygonSearch = polygonSearch
