6d59fe4113b5061d154a21b47cca2f8287916d7d
saaitt <saeid.firouzi@gmail.com>
2020-10-08 11:57:12 +0330

    data checking

--- routers/house.js
+++ routers/house.js
@@ -5,12 +5,10 @@ const houseHelper = require('../services/house')
 router.post('/nearby', getNearbyHouses)
 router.post('/polygonSearch', polygonSearch)
 
-
-
 async function getNearbyHouses(req, res) {
     try {
-        if(req.body.latitude == undefined || req.body.longitude ==undefined)
-            res.status(400).JSON({message: "Incomplete data."})
+        if (req.body.latitude == undefined || req.body.longitude == undefined)
+            res.status(400).JSON({ message: "Incomplete data." })
         let result = await houseHelper.nearbyHousesWithin500m(req.body.latitude, req.body.longitude)
         res.send(result)
     } catch (error) {
@@ -19,6 +17,11 @@ async function getNearbyHouses(req, res) {
 }
 async function polygonSearch(req, res) {
     try {
+        if (req.body.arrayOfLocations == undefined)
+            res.status(400).JSON({ message: "No data." })
+        if (req.body.arrayOfLocations.length < 3)
+            res.status(400).JSON({ message: "You need to pick more cordinates." })
+
         let result = await houseHelper.polygonSearch(req.body.arrayOfLocations)
         res.send(result)
     } catch (error) {
